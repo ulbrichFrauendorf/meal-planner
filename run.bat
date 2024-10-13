5521b98b23c26fc4bc610f4ae8f06472f16fe438
Ulbrich Frauendorf <invensys.za@gmail.com>
2024-10-14 01:02:44 +0200

    Update batch file

--- run.bat
+++ run.bat
@@ -1,27 +1,18 @@
 @echo off
-REM Get the directory of the batch file
+
 set script_dir=%~dp0
 
-REM Define relative paths to the solutions' directories from the batch file location
 set solution_auth_path=%script_dir%\MealPlannerAuthentication
 set solution_webapp_path=%script_dir%\MealPlannerMain
 
-REM Echo variables for debugging
-echo Script directory: %script_dir%
-echo First solution path: %solution_auth_path%
-echo Second solution path: %solution_webapp_path%
-
-REM Change directory to the auth solution directory
 echo Building and running auth project...
 cd /d %solution_auth_path%
 dotnet build -tl
 start dotnet dotnet watch run --project "src\Web\Web.csproj"
 
-REM Change directory to the webapp solution's directory
 echo Building and running webapp project...
 cd /d %solution_webapp_path%
 dotnet build -tl
 start dotnet watch run --project "src\Web\Web.csproj"
 
-REM Optional: Keep the terminal open
-pause
+Start-Process "https://localhost:44447"
